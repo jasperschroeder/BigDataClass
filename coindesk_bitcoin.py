cd4ba654638eaf9a22b5966799d343b933681d28
Jasper <59110008+jasperschroeder@users.noreply.github.com>
2021-05-01 17:42:23 +0200

    bpi

---
+++ coindesk_bitcoin.py
@@ -0,0 +1,23 @@
+# API coindesk
+import requests
+import json
+import pandas as pd
+
+
+# Feed in the YYYY-MM-DD format
+def get_bitcoin_prices(start, end):
+    url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
+    first = '?start=' + start
+    last = '&end=' + end
+    full_url = url + first + last
+    print(full_url)
+    r = requests.get(full_url)
+    data = json.loads(r.text)
+    prices = data['bpi']
+    return prices
+
+prices = get_bitcoin_prices('2016-01-01', '2021-04-30')
+
+df = pd.DataFrame(list(prices.items()), columns=['Date', 'BPI'])
+df = df.set_index('Date')
+df.to_csv('bpi.csv', index=True)
